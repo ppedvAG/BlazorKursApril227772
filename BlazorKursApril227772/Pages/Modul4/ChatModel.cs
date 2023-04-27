3ac5cea2890ef5a8bcce93f05c6ebcc3d9830ca3
Hannes Preishuber <hannesp@ppedv.de>
2023-04-27 17:02:07 +0200

    Chat Als Demo für Viewmodell per DI

---
+++ BlazorKursApril227772/Pages/Modul4/ChatModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorKursApril227772.Pages.Modul4
+{
+    public class ChatModel
+    {
+        public string Nachricht { get; set; }
+    }
+}
---
+++ BlazorKursApril227772/Pages/Modul4/NachrichtenVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorKursApril227772.Pages.Modul4
+{
+    public class NachrichtenVM
+    {
+        public Action OnAdd;
+        public List<ChatModel> ChatListe { get; set; }=new List<ChatModel>();
+        public void Add(string msg)
+        {
+
+            ChatListe.Add(new ChatModel() { Nachricht=msg });
+            OnAdd?.Invoke();
+        }
+    }
+}
