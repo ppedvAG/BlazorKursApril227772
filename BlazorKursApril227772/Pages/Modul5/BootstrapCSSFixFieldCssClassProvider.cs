05415f255aec26f6e18ad3ac4ef7e59a1a1f8ac7
Hannes Preishuber <hannesp@ppedv.de>
2023-04-28 11:47:29 +0200

    Bootstrap Validation CSS Fix is-

---
+++ BlazorKursApril227772/Pages/Modul5/BootstrapCSSFixFieldCssClassProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorKursApril227772.Pages.Modul5
+{
+    public class BootstrapCSSFixFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext,
+            in FieldIdentifier fieldIdentifier)
+        {
+            var isinvalid=editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isinvalid ? "is-invalid" : "is-valid";
+        }
+    }
+}
