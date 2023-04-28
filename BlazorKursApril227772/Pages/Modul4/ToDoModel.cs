9caffba9642233bec5a420120c390a55c482e398
Hannes Preishuber <hannesp@ppedv.de>
2023-04-28 09:28:15 +0200

    TodoListe von Rest Service mit Filter

---
+++ BlazorKursApril227772/Pages/Modul4/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorKursApril227772.Pages.Modul4
+{
+    public class ToDoModel
+    {
+
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
